7fb7115218e32ce502f114460f20225198e58f50
Jake Ashton <jakemaddenashton@gmail.com>
2022-10-18 20:54:23 -0700

    Clean up code and add comments

--- logic.js
+++ logic.js
@@ -1,11 +1,9 @@
 const board = [];
 const graph = [];
-
-const Square = (data, one, two, three, four, five, six, seven, eight) => {
-  return { data, one, two, three, four, five, six, seven, eight };
-};
+let index = [];
 
 const gameBoard = () => {
+  // create 8x8 game board
   for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
       board.push([i, j]);
@@ -14,20 +12,25 @@ const gameBoard = () => {
   return board;
 };
 
-// function isArrayInArray(arr, item) {
-//   const itemAsString = JSON.stringify(item);
+const addMoves = (moves, item) => {
+  const squareMoves = [];
 
-//   const contains = arr.some(function (ele) {
-//     return JSON.stringify(ele) === itemAsString;
-//   });
-//   return contains;
-// }
+  // check for valid moves within game board boundaries
+  moves.forEach((move) => {
+    if (move[0] >= 0 && move[0] <= 7 && move[1] >= 0 && move[1] <= 7) {
+      squareMoves.push(move);
+    }
+  });
+  index.push(squareMoves);
 
-// const establishedNodes = [];
+  // complete graph index at end of game board row, clear index to start new row
+  if (item[1] === 7) {
+    graph.push(index);
+    index = [];
+  }
+};
 
-let index = [];
-
-const addMoves = (item) => {
+const createMoves = (item) => {
   const one = [item[0] + 2, item[1] + 1];
   const two = [item[0] + 1, item[1] + 2];
   const three = [item[0] - 1, item[1] + 2];
@@ -37,23 +40,13 @@ const addMoves = (item) => {
   const seven = [item[0] + 1, item[1] - 2];
   const eight = [item[0] + 2, item[1] - 1];
   const moves = [one, two, three, four, five, six, seven, eight];
-  const squareMoves = [];
-  moves.forEach((move) => {
-    if (move[0] >= 0 && move[0] <= 7 && move[1] >= 0 && move[1] <= 7) {
-      squareMoves.push(move);
-    }
-  });
-  index.push(squareMoves);
-  if (item[1] === 7) {
-    graph.push(index);
-    index = [];
-  }
+  addMoves(moves, item);
 };
 
 const moves = () => {
   gameBoard();
   board.forEach((item) => {
-    addMoves(item);
+    createMoves(item);
   });
   return graph;
 };

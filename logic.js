f96b05650e97619d829f9ff29290d66c093a9d74
Jake Ashton <jakemaddenashton@gmail.com>
2022-10-18 20:44:34 -0700

    Add logic to create adjacency list representation of graph

--- logic.js
+++ logic.js
@@ -1,5 +1,5 @@
 const board = [];
-const movesBoard = [];
+const graph = [];
 
 const Square = (data, one, two, three, four, five, six, seven, eight) => {
   return { data, one, two, three, four, five, six, seven, eight };
@@ -8,7 +8,7 @@ const Square = (data, one, two, three, four, five, six, seven, eight) => {
 const gameBoard = () => {
   for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
-      board.push(Square([i, j]));
+      board.push([i, j]);
     }
   }
   return board;
@@ -25,40 +25,37 @@ const gameBoard = () => {
 
 // const establishedNodes = [];
 
+let index = [];
+
 const addMoves = (item) => {
-  const one = [item.data[0] + 2, item.data[1] + 1];
-  const two = [item.data[0] + 1, item.data[1] + 2];
-  const three = [item.data[0] - 1, item.data[1] + 2];
-  const four = [item.data[0] - 2, item.data[1] + 1];
-  const five = [item.data[0] - 2, item.data[1] - 1];
-  const six = [item.data[0] - 1, item.data[1] - 2];
-  const seven = [item.data[0] + 1, item.data[1] - 2];
-  const eight = [item.data[0] + 2, item.data[1] - 1];
+  const one = [item[0] + 2, item[1] + 1];
+  const two = [item[0] + 1, item[1] + 2];
+  const three = [item[0] - 1, item[1] + 2];
+  const four = [item[0] - 2, item[1] + 1];
+  const five = [item[0] - 2, item[1] - 1];
+  const six = [item[0] - 1, item[1] - 2];
+  const seven = [item[0] + 1, item[1] - 2];
+  const eight = [item[0] + 2, item[1] - 1];
   const moves = [one, two, three, four, five, six, seven, eight];
   const squareMoves = [];
   moves.forEach((move) => {
     if (move[0] >= 0 && move[0] <= 7 && move[1] >= 0 && move[1] <= 7) {
-      const validMove = board.find(
-        (element) => element.data[0] === move[0] && element.data[1] === move[1]
-      );
-      squareMoves.push(validMove);
-    } else squareMoves.push(null);
+      squareMoves.push(move);
+    }
   });
-  // squareData.forEach((item) => {
-  //   addMoves(item);
-  // });
-  item = Square(item.data, ...squareMoves);
-  return item;
+  index.push(squareMoves);
+  if (item[1] === 7) {
+    graph.push(index);
+    index = [];
+  }
 };
 
 const moves = () => {
   gameBoard();
   board.forEach((item) => {
-    const newItem = addMoves(item);
-    board.shift();
-    board.push(newItem);
+    addMoves(item);
   });
-  return board;
+  return graph;
 };
 
-const graph = moves();
+const wow = moves();
